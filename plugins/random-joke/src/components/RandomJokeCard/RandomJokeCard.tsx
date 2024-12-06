ed8937d94e2ae8584cc283bc56832129ced66a43
Youngdon Jung <youngdon.jung@lotte.net>
2024-12-06 16:07:49 +0900

    Sample InfoCard

--- plugins/random-joke/src/components/RandomJokeCard/RandomJokeCard.tsx
+++ plugins/random-joke/src/components/RandomJokeCard/RandomJokeCard.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useAsync } from 'react-use';
 import { InfoCard } from '@backstage/core-components';
+import { fetchApiRef, useApi } from '@backstage/core-plugin-api';
 
 export const RandomJokeCard = () => {
-    return <InfoCard> Backstage is easy setup! </InfoCard>
+    const fetchApi = useApi(fetchApiRef);
+
+    const state = useAsync(async () => {
+        const response = await fetchApi.fetch('https://icanhazdadjoke.com/',{
+            headers:{
+                Accept: 'application/json'
+            }
+        })
+        const joke = await response.text();
+        return joke
+    }, []);
+
+
+    return (
+        <InfoCard>
+            {state.loading
+            ? <div>Loading...</div>
+            : state.error
+                ? <div>Error: {state.error.message}</div>
+                :<div>{state.value}</div>
+            }
+        </InfoCard>
+    )
 }
